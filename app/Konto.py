d78addde48b782593d784665ad7387feb3005b64
Karol-2 <97386279+Karol-2@users.noreply.github.com>
2022-10-22 22:28:06 +0200

    feature 1-5

--- app/Konto.py
+++ app/Konto.py
@@ -1,7 +1,21 @@
 class Konto:
-    def __init__(self,imie,nazwisko,pesel):
+    def __init__(self, imie, nazwisko, pesel, kod_rabatowy=None):
         self.imie = imie
         self.nazwisko = nazwisko
-        self.pesel = pesel
+        self.pesel = None
         self.saldo = 0
-        pass
+
+        self.sprawdz_pesel(pesel)
+        self.sprawdz_kod_rabatowy(kod_rabatowy)
+
+    def sprawdz_pesel(self, pesel):
+        if len(pesel) == 11:
+            self.pesel = pesel
+        else:
+            self.pesel = "Niepoprawny pesel!"
+
+    def sprawdz_kod_rabatowy(self, kod):
+        if kod is not None:
+            if len(kod) == 8 and kod[0:5] == "PROM_":  # warunki feature4
+                if int(self.pesel[0:2]) > 60:  # warunki feature 5
+                    self.saldo += 50
