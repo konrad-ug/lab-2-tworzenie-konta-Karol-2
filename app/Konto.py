c1ca828630c406716ddbbf18a5d12282bc6c4946
Karol-2 <97386279+Karol-2@users.noreply.github.com>
2022-10-22 19:03:33 +0200

    test

--- app/Konto.py
+++ app/Konto.py
@@ -1,3 +1,7 @@
 class Konto:
-    def __init__(self):
+    def __init__(self,imie,nazwisko,pesel):
+        self.imie = imie
+        self.nazwisko = nazwisko
+        self.pesel = pesel
+        self.saldo = 0
         pass
--- app/tests/test_create_bank_account.py
+++ app/tests/test_create_bank_account.py
@@ -4,10 +4,11 @@ from ..Konto import Konto
 
 class TestCreateBankAccount(unittest.TestCase):
 
+    imie = "Dariusz"
     def test_tworzenie_konta(self):
-        pierwsze_konto = Konto("Dariusz", "Januszewski")
+        pierwsze_konto = Konto(self.imie, "Januszewski", "12345678912")
         self.assertEqual(pierwsze_konto.imie, "Dariusz", "Imie nie zostało zapisane!")
         self.assertEqual(pierwsze_konto.nazwisko, "Januszewski", "Nazwisko nie zostało zapisane!")
         self.assertEqual(pierwsze_konto.saldo, 0, "Saldo nie jest zerowe!")
+        self.assertEqual(pierwsze_konto.pesel, "12345678912","Zly pesel")
 
-    #tutaj proszę dodawać nowe testy
