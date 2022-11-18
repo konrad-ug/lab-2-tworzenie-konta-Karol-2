c3c1127e0e552035f9011c2f91ffbb639cd5f323
Karol Krawczykiewicz <26karol26@gmail.com>
2022-11-18 15:28:44 +0100

    lab05 parametrized

--- app/tests/test_kredyty.py
+++ app/tests/test_kredyty.py
@@ -2,6 +2,8 @@ import unittest
 from app.Konto import Konto
 from app.KontoFirmowe import KontoFirmowe
 
+from parameterized import parameterized
+
 
 class TestKredyty(unittest.TestCase):
     imie = "Dariusz"
@@ -14,33 +16,15 @@ class TestKredyty(unittest.TestCase):
     def setUp(self):
         self.konto = Konto(self.imie, self.nazwisko, self.pesel)
 
-    def test_5_przelewy(self):
-        self.konto.historia = [-100, 100, 100, 100, 600]
-        czy_przyznany = self.konto.zaciagnij_kredyt(500)
-        self.assertTrue(czy_przyznany)
-        self.assertEqual(self.konto.saldo, 500, "Kredyt nie dodany do konta")
-
-    def test_pusta_historia(self):
-        self.konto.saldo = 0
-        self.konto.historia = []
-        czy_przyznany = self.konto.zaciagnij_kredyt(500)
-        self.assertFalse(czy_przyznany, "Udzielono kredytu wbrew zasadom")
-        self.assertEqual(self.konto.saldo, 0, "Udzielono kredytu wbrew zasadom")
-
-    def test_za_krotka_historia(self):
-        self.konto.historia = [-100, 50]
-        czy_przyznany = self.konto.zaciagnij_kredyt(500)
-        self.assertFalse(czy_przyznany, "Udzielono kredytu wbrew zasadom")
-        self.assertEqual(self.konto.saldo, 0, "Udzielono kredytu wbrew zasadom")
-
-    def test_same_wychodzace(self):
-        self.konto.historia = [-100, -200, -4555, -412, -111]
-        czy_przyznany = self.konto.zaciagnij_kredyt(500)
-        self.assertFalse(czy_przyznany, "Udzielono kredytu wbrew zasadom")
-        self.assertEqual(self.konto.saldo, 0, "Udzielono kredytu wbrew zasadom")
-
-    def test_suma_mniejsza_niz_kredyt(self):
-        self.konto.historia = [50, 30, 20, 10, 50, 10, 11]
-        czy_przyznany = self.konto.zaciagnij_kredyt(2000000)
-        self.assertFalse(czy_przyznany, "Udzielono kredytu osobie z zbyt małymi wpłatami")
-        self.assertEqual(self.konto.saldo, 0, "Udzielono kredytu wbrew zasadom")
+    @parameterized.expand([
+        ([-100, 100, 100, 100, 600], 500, True, 500),
+        ([], 500, False, 0),
+        ([-100, 50], 500, False, 0),
+        ([-100, -200, -4555, -412, -111], 500, False, 0),
+        ([50, 30, 20, 10, 50, 10, 11], 200000, False, 0)
+    ])
+    def test_5_przychodzace_przelewy(self, historia, kwota_kredytu, oczekiwany_wynik, oczekiwane_saldo):
+        self.konto.historia = historia
+        czy_przyznany = self.konto.zaciagnij_kredyt(kwota_kredytu)
+        self.assertEqual(czy_przyznany, oczekiwany_wynik)
+        self.assertEqual(self.konto.saldo, oczekiwane_saldo)
