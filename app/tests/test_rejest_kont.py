446f2d59ba0fa1a5785119e0cb9ccc05d582179c
Karol-2 <97386279+Karol-2@users.noreply.github.com>
2022-11-26 17:43:18 +0100

    lab06 fix

--- app/tests/test_rejest_kont.py
+++ app/tests/test_rejest_kont.py
@@ -17,16 +17,21 @@ class TestRejestrKont(unittest.TestCase):
     def test_1_dodawanie_drugiego_konta(self):
         konto = Konto(self.imie,self.nazwisko,self.pesel)
         RejestrKont.dodaj_konto(konto)
-        self.assertEqual(RejestrKont.ile_kont(),2)
+        self.assertEqual(RejestrKont.ile_kont(),2,"nie dodano drugiego konta")
 
     def test_2_dodawanie_trzeciego_konta(self):
         konto = Konto(self.imie,self.nazwisko,self.pesel)
         RejestrKont.dodaj_konto(konto)
-        self.assertEqual(RejestrKont.ile_kont(),3)
+        self.assertEqual(RejestrKont.ile_kont(),3,"nie dodano trzeciego konta")
 
     def test_3_szukanie_konta(self):
         szukany_pesel = "89495784261"
-        self.assertEqual(RejestrKont.wyszukaj_konto(szukany_pesel).pesel,szukany_pesel)
+        self.assertEqual(RejestrKont.wyszukaj_konto(szukany_pesel).pesel,szukany_pesel,"błąd w szukaniu pesela")
+
+    def test_4_szukanie_nieistniejacego_konta(self):
+        szukany_pesel = "11111111111"
+        self.assertEqual(RejestrKont.wyszukaj_konto(szukany_pesel), None,"znaleziono błędny pesel")
+
 
     @classmethod
     def tearDownClass(cls):
